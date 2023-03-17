70cb6b6abe7998c099f1cf0ab4ebd54d41cd75d6
CJ Hare <CjHare@users.noreply.github.com>
2023-03-17 20:30:09 +1000

    chore: hardhat networks with manual mining (#3)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,47 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 
-module.exports = {
-  solidity: "0.8.18",
-};
+/**
+ * 0.8.18 was the latest version supported on config authoring
+ *
+ * https://hardhat.org/hardhat-runner/docs/reference/solidity-support
+ */
+export default {
+  networks: {
+    hardhat: {
+      chainId: 33133,
+      allowUnlimitedContractSize: false,
+      loggingEnabled: false,
+      mining: {
+        auto: false,
+        interval: 0
+      }
+    },s
+    local: {
+      url: 'http://localhost:8545',
+      chainId: 33133,
+      allowUnlimitedContractSize: false,
+      loggingEnabled: true,
+      mining: {
+        auto: false,
+        interval: 0
+      }
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.8.18'
+      }
+    ]
+  }
+}
